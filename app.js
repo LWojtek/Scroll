42d854aa15113862eb1627c5e152f3dec792057e
Piecuch <marcinszulc@hotmail.com>
2020-10-19 17:08:51 +0200

    getElement's'ById fix

--- app.js
+++ app.js
@@ -32,20 +32,20 @@ navToggle.addEventListener('click', () => {
 
 });
 
-const navbar = document.getElementsById = ('nav');
+const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
 
 // ********** fixed navbar ************
 
 window.addEventListener('scroll', () => {
-    // const scrollHeight = window.pageYOffset;
-    // const navHeight = navbar.getBoundingClientRect().height;
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
 
-    // if (scrollHeight > navHeight) {
-    //     navbar.classList.add('fixed-nav');
-    // } else {
-    //     navbar.classList.remove('fixed-nav');
-    // }
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
 
     if (window.pageYOffset > 830) {
         topLink.classList.add('show-link');
